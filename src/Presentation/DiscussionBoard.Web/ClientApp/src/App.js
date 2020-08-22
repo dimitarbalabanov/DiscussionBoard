86083d5206336e04506d60236889f69effea31c8
Dimitar Balabanov <dimitar.balabanov@gmail.com>
2020-08-22 13:47:40 +0300

    I will be using the Clean architecture pattern. Added the web ui project in the presentation folder - the api and an initial react app bootstrapped with create-react-app.

---
+++ src/Presentation/DiscussionBoard.Web/ClientApp/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  let [state, setState] = useState("");
+
+    useEffect(() => {
+        fetch('api/sample')
+            .then(response => response.json())
+            .then(data => setState(data))
+            .catch(error=> console.log(error));
+    }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Sample controller says: { state }
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Presentation/DiscussionBoard.Web/Controllers/Sample.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DiscussionBoard.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Sample : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(JsonConvert.SerializeObject("bla bla"));
+        }
+    }
+}
