5782f2658213a1ecda10e9f37b648b2ce17d33d5
Dimitar Balabanov <dimitar.balabanov@gmail.com>
2020-09-05 19:50:59 +0300

    Cleaned up the react project. Added all the npm packages I plan on using - material-ui for ui, axios for fetching, formik for forms together with yup for form validation, react-router-dom for routing, react-redux and redux for state management and redux-thunk for async dispatching.

--- src/Presentation/DiscussionBoard.Web/ClientApp/src/App.js
+++ src/Presentation/DiscussionBoard.Web/ClientApp/src/App.js
@@ -1,32 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React from 'react';
 
 function App() {
-  let [state, setState] = useState([]);
-
-    useEffect(() => {
-        //fetch('api/forums'
-        //    //, {
-        //    //headers: {
-        //    //    'Authorization': 'Bearer ',
-        //    //}
-        //    //}
-        //)
-        //    .then(response => response.json())
-        //    .then(data => setState(data.forums))
-        //    .catch(error=> console.log(error));
-    }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-          <ul>
-            {
-              //state.map(x => <li key={x.id}><strong>Title:</strong> {x.title} <strong>Description:</strong> {x.description} </li>)
-            }
-          </ul>
-      </header>
-    </div>
+    <h1>Home</h1>
   );
 }
 
